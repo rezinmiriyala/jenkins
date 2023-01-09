22327e424fbb0aea20ee7f5333ea9f24d1a01315
miriyalabittu <miriyalabittu@localhost>
2023-01-09 16:03:53 +0530

    initial commit

---
+++ jenkins/src/main/java/jenkdemo/Test1.java
@@ -0,0 +1,12 @@
+package jenkdemo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void m1()
+{
+	Reporter.log("hii",true);
+}
+}
---
+++ jenkins/src/main/java/jenkdemo/Test2.java
@@ -0,0 +1,13 @@
+package jenkdemo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void m2()
+	{
+		Reporter.log("love you",true);
+	}
+
+}
